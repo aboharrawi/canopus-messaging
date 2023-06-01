ebb53e01cbb72cdb31c893d2180bdb4a73a184ab
mharawi <mharawi@atypon.com>
2023-06-01 09:12:37 +0300

    Kafka consumer implementation for streaming with json payload

--- src/main/java/com/canopus/messaging/consumer/Consumer.java
+++ src/main/java/com/canopus/messaging/consumer/Consumer.java
@@ -3,7 +3,10 @@ package com.canopus.messaging.consumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class Consumer {
@@ -11,7 +14,7 @@ public class Consumer {
     private final Logger logger = LoggerFactory.getLogger(Consumer.class);
 
     @KafkaListener(topics = "canopus-live-stream", groupId = "canopus-streaming")
-    public void consume(byte[] message) {
-        logger.info(String.format("Received byte[] message with length %d", message.length));
+    public void consume(Message<Map<String, String>> message) {
+        logger.info(String.format("Received message %s", message));
     }
 }
